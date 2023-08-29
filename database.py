c3604ebbeb5a1022210325d147a42e8cf0566f7e
s0569302 <s0569302@htw-berlin.de>
2023-08-29 13:07:34 +0200

    structure updated

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./cv.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./src/db/cv.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}) #check_same_thread only for litesql 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
